820a48df13b14d04d8c0dcead7dceab994b49263
Nitiraj Solanki <nitirajsolanki@outlook.com>
2025-01-20 13:24:25 -0500

    Added a CustomImageFolder class to split bacteria and virus files. Added a separate transformation to the val and test folder. using the Resnet34 with IMAGENET1K_V1 model because I found that i brings the most efficient result. 92.65% isn't bad
    Resnet50 probably would result in better result but I don't have enough GPU power.

--- Models/pneumoniatracker.py
+++ Models/pneumoniatracker.py
@@ -21,6 +21,9 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from Models.HelperFunction import helperFunctions as hp
 import torchvision.models as models 
+from PIL import Image
+import timm
+
 
 folder_file_path = Path.cwd()/'dataset'/'chest_xray'
 folder_location = Path.cwd()/'dataset'
@@ -33,15 +36,21 @@ else:
 torch.manual_seed(42)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-my_transforms = transforms.Compose([
+
+train_transforms = transforms.Compose([
+    transforms.RandomRotation(20),  # Randomly rotate the image within a range of (-20, 20) degrees
+    transforms.RandomHorizontalFlip(p=0.5),  # Randomly flip the image horizontally with 50% probability
+    transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),  # Randomly change the brightness, contrast, saturation, and hue
+    transforms.RandomApply([transforms.RandomAffine(0, translate=(0.1, 0.1))], p=0.5),  # Randomly apply affine transformations with translation
+    transforms.RandomApply([transforms.RandomPerspective(distortion_scale=0.2)], p=0.5),  # Randomly apply perspective transformations
+    transforms.Resize(size=(224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                            std=[0.229, 0.224, 0.225])
+])
+test_val_transforms = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    transforms.RandomHorizontalFlip(p=0.5),
-    transforms.RandomRotation(degrees=45),
-    transforms.RandomAffine(degrees=15, translate = None, scale=(0.9,1.1), shear=None),
-    transforms.RandomCrop(size = (224, 224)),
-    transforms.RandomGrayscale(p=0.2),
-    transforms.ColorJitter(contrast=0.2),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
@@ -49,13 +58,13 @@ train_path = Path.cwd()/'dataset'/'chest_xray'/"chest_xray"/'train'
 test_path = Path.cwd()/'dataset'/'chest_xray'/"chest_xray"/'test'
 val_path = Path.cwd()/'dataset'/'chest_xray'/"chest_xray"/'val'
 
-train_dataset = datasets.ImageFolder(root=train_path, transform=my_transforms)
-test_dataset = datasets.ImageFolder(root=test_path, transform=my_transforms)
-val_dataset = datasets.ImageFolder(root=val_path, transform=my_transforms)
+train_dataset = datasets.ImageFolder(root=train_path, transform=train_transforms)
+test_dataset = datasets.ImageFolder(root=test_path, transform=test_val_transforms)
+val_dataset = datasets.ImageFolder(root=val_path, transform=test_val_transforms)
 
 train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
-val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=True)
+test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+val_dataloader = DataLoader(val_dataset, batch_size=64, shuffle=False)
 
 plt.figure(figsize=(10, 10))
 plt.imshow(imread(f"{Path.cwd()}/dataset/chest_xray/chest_xray/train/NORMAL/IM-0115-0001.jpeg"))
@@ -100,13 +109,15 @@ class XrayModel(nn.Module):
         x = self.classifier(x)
         return x
 
-model = XrayModel()
+# model = models.efficientnet_v2_s(weights=models.EfficientNet_V2_S_Weights.IMAGENET1K_V1)
+print(device)
+model = models.resnet34(weights = models.ResNet34_Weights.IMAGENET1K_V1).to(device)
 loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001, weight_decay=1e-5)
-scheduler = torch.optim.lr_scheduler.StepLR(optimizer, T_max=10, eta_min=0.001*0.1)
+optimizer = torch.optim.Adam(params=model.parameters(), lr=0.001)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=5, eta_min=0.001*0.1)
 
 def train_and_eval():
-    trained_model, _ = hp.train_and_evaluate_2d(model, train_dataloader, test_dataloader, loss, optimizer,scheduler,device, 6, 1)
+    trained_model, _ = hp.train_and_evaluate_2d(model, train_dataloader, test_dataloader, loss, optimizer,scheduler,device, 5, 1)
     model_save_path = Path.cwd()/'Models'/'SavedModels'/'PneumoniaTrackerModel.pth'
     torch.save(trained_model.state_dict(), model_save_path)
     print(f"Model saved at: {model_save_path}")
